099b19d186c9585291fcced341b47ea297bbdf6b
Jay Sylvester <jay@jaysylvester.com>
2015-05-02 18:12:54 -0400

    Old migration script cleanup

--- migration/sql/topics-3.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "public"."topics" ADD UNIQUE ("url");
-ALTER TABLE "public"."topics" alter column "firstPostID" set not null;
-ALTER TABLE "public"."topics" alter column "lastPostID" set not null;
-ALTER TABLE "public"."topics" alter column "titleHtml" set not null;
--- migration/sql/users-2.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE "public"."users" ADD UNIQUE ("url");
