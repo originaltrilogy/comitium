46f2d7c106346f489611c6da4fb81a2167fbc5f9
Jay Sylvester <jay@jaysylvester.com>
2015-08-11 02:37:00 -0400

    Fixed IP helper output

--- app/toolbox/helpers.js
+++ app/toolbox/helpers.js
@@ -44,7 +44,9 @@ function compareHash(str, hash, emitter) {
 
 
 function ip(request) {
-  return request.headers['x-forwarded-for'] || request.connection.remoteAddress || request.socket.remoteAddress || ( request.connection.socket ? request.connection.socket.remoteAddress : 'undefined' );
+  var address = request.headers['x-forwarded-for'] || request.connection.remoteAddress || request.socket.remoteAddress || ( request.connection.socket ? request.connection.socket.remoteAddress : 'undefined' );
+
+  return address.replace('::ffff:', '');
 }
 
 
