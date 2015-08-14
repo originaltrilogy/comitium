670530a24e316f2b817f141cb6799f2b5367b999
Jay Sylvester <jay@jaysylvester.com>
2015-08-14 04:59:15 -0400

    #105 - User logs Can't cache the sign out controller due to the call to the user model

--- app/patterns/controllers/sign-out.js
+++ app/patterns/controllers/sign-out.js
@@ -16,11 +16,6 @@ function handler(params, context, emitter) {
 
   emitter.emit('ready', {
     view: params.url.reason || 'sign-out',
-    cache: {
-      controller: {
-        directives: ['cookie', 'session']
-      }
-    },
     cookie: {
       comitium_id: {
         expires: 'now'
