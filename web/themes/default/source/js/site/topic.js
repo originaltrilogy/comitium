864d0eb03e7c3d2b1e1407dd3f7edec9f4a9ef9b
Jay Sylvester <jay@jaysylvester.com>
2016-04-10 20:33:10 -0400

    Layout in progress

--- web/themes/default/source/js/site/topic.js
+++ web/themes/default/source/js/site/topic.js
@@ -1,84 +1,86 @@
 CF.topic = ( function (Modernizr, CF) {
   'use strict';
 
-	var actions = {
+  var actions, methods;
 
-      handler: function () {
-        // if ( document.querySelectorAll('main nav.topic.actions li').length > 2 ) {
-        //   methods.topicMenu();
-        // }
-        var form = document.querySelector('#quick-reply-form');
+	actions = {
 
-        if ( form && !CF.global.hasClass(form.parentNode, 'quote') ) {
-          methods.postContent();
-        }
-      },
+    handler: function () {
+      // if ( document.querySelectorAll('main nav.topic.actions li').length > 2 ) {
+      //   methods.topicMenu();
+      // }
+      var form = document.querySelector('#quick-reply-form');
 
-      start: function () {
-        // CF.global.ajaxFormBinding({
-        //   formSelector: '#topic-write-form'
-        // });
+      if ( form && !CF.global.hasClass(form.parentNode, 'quote') ) {
         methods.postContent();
-      },
-
-      reply: function () {
-        // CF.global.ajaxFormBinding({
-        //   formSelector: '#topic-reply-form'
-        // });
-        var form = document.querySelector('#topic-reply-form');
-
-        if ( form && !CF.global.hasClass(form.parentNode, 'quote') ) {
-          methods.postContent();
-        }
       }
+    },
 
+    start: function () {
+      // CF.global.ajaxFormBinding({
+      //   formSelector: '#topic-write-form'
+      // });
+      methods.postContent();
+    },
+
+    reply: function () {
+      // CF.global.ajaxFormBinding({
+      //   formSelector: '#topic-reply-form'
+      // });
+      var form = document.querySelector('#topic-reply-form');
+
+      if ( form && !CF.global.hasClass(form.parentNode, 'quote') ) {
+        methods.postContent();
+      }
+    }
+
+	};
+
+  methods = {
+
+		init: function () {
+      // methods.postContent();
 		},
 
-    methods = {
+    // topicMenu: function () {
+    //   var menu = document.querySelector('main nav.topic.actions ul'),
+    //       moreButton = document.createElement('li'),
+    //       moreAnchor = document.createElement('a');
 
-			init: function () {
-        // methods.postContent();
-			},
+    //   moreButton.className = 'more';
+    //   moreButton.appendChild(moreAnchor);
+    //   moreAnchor.appendChild(document.createTextNode('More...'));
+    //   menu.appendChild(moreButton);
 
-      // topicMenu: function () {
-      //   var menu = document.querySelector('main nav.topic.actions ul'),
-      //       moreButton = document.createElement('li'),
-      //       moreAnchor = document.createElement('a');
+    //   CF.global.menu({
+    //     menu: 'main nav.topic.actions',
+    //     trigger: 'main nav.topic.actions li.more a',
+    //     position: 'right',
+    //     clone: true,
+    //     keepClass: false
+    //   });
 
-      //   moreButton.className = 'more';
-      //   moreButton.appendChild(moreAnchor);
-      //   moreAnchor.appendChild(document.createTextNode('More...'));
-      //   menu.appendChild(moreButton);
+    // },
 
-      //   CF.global.menu({
-      //     menu: 'main nav.topic.actions',
-      //     trigger: 'main nav.topic.actions li.more a',
-      //     position: 'right',
-      //     clone: true,
-      //     keepClass: false
-      //   });
+    postContent: function () {
+      var postContent = document.getElementById('post-content'),
+          postContentText = postContent ? postContent.value : '';
 
-      // },
-
-      postContent: function () {
-        var postContent = document.getElementById('post-content'),
-            postContentText = postContent ? postContent.value : '';
-
-        if ( postContent ) {
-          postContent.addEventListener('focus', function (e) {
-            if ( postContent.value === postContentText ) {
-              postContent.value = '';
-            }
-          });
-          postContent.addEventListener('blur', function (e) {
-            if ( postContent.value === '' ) {
-              postContent.value = postContentText;
-            }
-          });
-        }
+      if ( postContent ) {
+        postContent.addEventListener('focus', function (e) {
+          if ( postContent.value === postContentText ) {
+            postContent.value = '';
+          }
+        });
+        postContent.addEventListener('blur', function (e) {
+          if ( postContent.value === '' ) {
+            postContent.value = postContentText;
+          }
+        });
       }
+    }
 
-    };
+  };
 
 	//	Public methods
 	return {
