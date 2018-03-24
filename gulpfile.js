bab96ecc48998dc9bc4844360a89a7d750ff5ff4
Jay Sylvester <jay@jaysylvester.com>
2018-03-24 16:18:01 -0400

    Cleaned up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-'use strict';
+'use strict'
 
 var autoprefixer  = require('autoprefixer'),
     gulp          = require('gulp'),
@@ -9,7 +9,7 @@ var autoprefixer  = require('autoprefixer'),
     postcss       = require('gulp-postcss'),
     sass          = require('gulp-sass'),
     sourcemaps    = require('gulp-sourcemaps'),
-    uglify        = require('gulp-uglify');
+    uglify        = require('gulp-uglify')
 
 var themes = [
       {
@@ -30,7 +30,7 @@ var themes = [
       // }
     ],
     devTasks = [],
-    prodTasks = [];
+    prodTasks = []
 
 
 function css(options) {
@@ -42,20 +42,20 @@ function css(options) {
                .pipe(sourcemaps.write(''))
              .pipe(gulp.dest('web/themes/' + options.theme + '/min'))
              .pipe(filter('**/*.css'))
-             .pipe(livereload());
+             .pipe(livereload())
 }
 
 
-themes.forEach( function (item, index, array) {
-  prodTasks[index] = 'css' + item.name;
+themes.forEach( function (item, index) {
+  prodTasks[index] = 'css' + item.name
 
   gulp.task('css' + item.name, function () {
-    css({ theme: item.path });
-  });
-});
+    css({ theme: item.path })
+  })
+})
 
-devTasks.push('jsDev');
-prodTasks.push('jsProd');
+devTasks.push('jsDev')
+prodTasks.push('jsProd')
 
 gulp.task('jsDev', function () {
   return gulp.src(['web/themes/default/source/js/lib/modernizr-dev.js',
@@ -68,8 +68,8 @@ gulp.task('jsDev', function () {
                .pipe(concat('debug.js'))
                .pipe(sourcemaps.write(''))
              .pipe(gulp.dest('web/themes/default/min'))
-             .pipe(livereload());
-});
+             .pipe(livereload())
+})
 
 gulp.task('jsProd', function () {
   return gulp.src(['web/themes/default/source/js/lib/modernizr-prod.js',
@@ -82,31 +82,30 @@ gulp.task('jsProd', function () {
                .pipe(concat('production.js'))
                .pipe(sourcemaps.write(''))
              .pipe(gulp.dest('web/themes/default/min'))
-             .pipe(livereload());
-});
+             .pipe(livereload())
+})
 
 gulp.task('views', function () {
-  livereload.reload();
-  return;
-});
+  livereload.reload()
+  return
+})
 
 gulp.task('watch', function() {
-  livereload.listen();
-  themes.forEach( function (item, index, array) {
-    gulp.watch('web/themes/default/source/scss/**/**.scss', ['css' + item.name]);
-  });
-  themes.forEach( function (item, index, array) {
+  livereload.listen()
+  themes.forEach( function (item) {
+    gulp.watch('web/themes/default/source/scss/**/**.scss', ['css' + item.name])
+  })
+  themes.forEach( function (item) {
     if ( item.name !== 'Default') {
-      gulp.watch('web/themes/' + item.path + '/source/scss/**/**.scss', ['css' + item.name]);
+      gulp.watch('web/themes/' + item.path + '/source/scss/**/**.scss', ['css' + item.name])
     }
-  });
-  gulp.watch('web/themes/default/source/js/**/**.js', ['jsDev', 'jsProd']);
-  gulp.watch('app/patterns/views/**/**.jade', ['views']);
-  gulp.watch('web/**/**.jade', ['views']);
-});
+  })
+  gulp.watch('web/themes/default/source/js/**/**.js', ['jsDev', 'jsProd'])
+  gulp.watch('app/patterns/views/**/**.jade', ['views'])
+  gulp.watch('web/**/**.jade', ['views'])
+})
 
-// gulp.task('default', ['css']);
-gulp.task('default', ['watch']);
-gulp.task('dev', devTasks);
-gulp.task('prod', prodTasks);
-gulp.task('all', devTasks.concat(prodTasks));
+gulp.task('default', ['watch'])
+gulp.task('dev', devTasks)
+gulp.task('prod', prodTasks)
+gulp.task('all', devTasks.concat(prodTasks))
