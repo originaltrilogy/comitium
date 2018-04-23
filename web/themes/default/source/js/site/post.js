5fda2bf0ca29e7a2008f7b3bf5f790a258f0ae68
Jay Sylvester <jay@jaysylvester.com>
2018-04-23 00:44:12 -0400

    Fire default topic scripts when post controller points to a post within a topic

--- web/themes/default/source/js/site/post.js
+++ web/themes/default/source/js/site/post.js
@@ -5,13 +5,18 @@ CF.post = ( function () {
 
 		init: function () {
       CF.global.collapseQuotes()
+		},
+		// If the post controller is fired to send the user directly to a post, run the default topic scripts
+		topic: function () {
+			CF.topic.handler()
 		}
 
   }
 
 	//	Public methods
 	return {
-		init: methods.init
+		init: methods.init,
+		topic: methods.topic
 	}
 
 })(CF)
