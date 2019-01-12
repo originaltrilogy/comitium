dd7a203735c94081f540bd85d8bf51f6ee0ba75f
Jay Sylvester <jay@jaysylvester.com>
2019-01-12 15:15:10 -0500

    QA app start

--- app/start-qa.js
+++ app/start-qa.js
@@ -1,10 +1,10 @@
 // app start
 
-'use strict';
+'use strict'
 
-global.app = require('citizen');
+global.app = require('citizen')
 
-var fs = require('fs');
+var fs = require('fs')
 
 app.toolbox = {
   // Native modules
@@ -28,42 +28,42 @@ app.toolbox = {
         },
         toFile: true,
         file: 'email.txt'
-      });
+      })
     }
   },
   moment: require('moment-timezone'),
   numeral: require('numeral'),
   pg: require('pg'),
   slug: require('slug')
-};
+}
 
 // Overwrite pg's default date handler to convert to GMT
 app.toolbox.pg.types.setTypeParser(1114, function (stringValue) {
-  return new Date(Date.parse(stringValue + ' +0000')).toISOString();
-});
+  return new Date(Date.parse(stringValue + ' +0000')).toISOString()
+})
 // Create a connection pool
-app.toolbox.dbPool = new app.toolbox.pg.Pool(app.config.comitium.db);
+app.toolbox.dbPool = new app.toolbox.pg.Pool(app.config.comitium.db)
 // Log errors in the connection pool
-app.toolbox.dbPool.on('error', function (err, client) {
+app.toolbox.dbPool.on('error', function (err) {
   app.log({
     type: 'error',
     label: 'Database pool error',
     contents: err
-  });
-});
+  })
+})
 
 // Overwrite slug's character map to avoid funky URLs
-app.toolbox.slug.charmap['~'] = '-';
-app.toolbox.slug.charmap['_'] = '-';
-app.toolbox.slug.charmap['---'] = '-';
-app.toolbox.slug.charmap['--'] = '-';
+app.toolbox.slug.charmap['~'] = '-'
+app.toolbox.slug.charmap['_'] = '-'
+app.toolbox.slug.charmap['---'] = '-'
+app.toolbox.slug.charmap['--'] = '-'
 
 // Static resources
 app.resources = {
   images: {
     defaultAvatar: fs.readFileSync(app.config.citizen.directories.app + '/resources/images/default-avatar.jpg')
   }
-};
+}
 
 // Start the server
-app.start();
+app.start()
