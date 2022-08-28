c4c2aa6802e0c5e91f83f08fd209983abd02697c
Jay Sylvester <jay@jaysylvester.com>
2022-08-28 18:22:38 -0400

    Cleaned up header logic

--- app/patterns/controllers/+_header.js
+++ app/patterns/controllers/+_header.js
@@ -1,27 +1,23 @@
 // _header controller
 
 export const handler = async (params) => {
-  if ( !params.session.username ) {
-    return {
-      public: {
-        logo: app.resources.images.logoHorizontal
-      }
-    }
-  } else {
-    let [
+  let unreadTopics, unreadPrivateTopics
+
+  if ( params.session.username ) {
+    [
       unreadTopics,
       unreadPrivateTopics
     ] = await Promise.all([
       app.models.subscriptions.unread({ userID: params.session.user_id }),
       app.models['private-topics'].unread({ userID: params.session.user_id })
     ])
+  }
 
-    return {
-      public: {
-        unread: {
-          topics: unreadTopics,
-          privateTopics: unreadPrivateTopics
-        }
+  return {
+    public: {
+      unread: {
+        topics: unreadTopics,
+        privateTopics: unreadPrivateTopics
       }
     }
   }
